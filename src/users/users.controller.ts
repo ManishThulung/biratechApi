ab006e58af322053aa4456095e9e3fcfb4635c44
Manish Thulung <manish.thulung58@gmail.com>
2023-03-20 22:13:27 +0545

    users setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  UsePipes,
+  ValidationPipe,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+import { RegisterDto } from './dto/user.dto';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Get()
+  getUsers() {
+    return this.usersService.getUsers();
+  }
+
+  @UsePipes(new ValidationPipe())
+  @Post()
+  createUser(@Body() user: RegisterDto) {
+    return this.usersService.registerUser(user);
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RegisterDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  getUsers() {
+    return this.userRepository.find();
+  }
+
+  async registerUser(user: RegisterDto) {
+    try {
+      await this.userRepository.save(user);
+      return 'User Registered';
+    } catch (error) {
+      throw new HttpException('Error occurred!', HttpStatus.BAD_REQUEST);
+    }
+  }
+}
